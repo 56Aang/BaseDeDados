61bb859a277897501e502475bd9115edc23a0495
João Guedes <joao@MBP-de-Joao.lan>
2020-12-03 21:49:18 +0000

    cliente

--- povoamento.sql
+++ povoamento.sql
@@ -13,9 +13,29 @@ VALUES
     ('França', 'Roissy-en-France',  '95700', 'Aeroporto de Roissy-Charles-de-Gaulle'),
     ('Alemanha', 'Frankfurt',  '60547', 'Aeroporto de Frankfurt am Main');
  
- -- aviao
+ -- aviao faltam as classes
 INSERT INTO aviao(Nome, Numero_maximo_de_passageiros,Tara,Companhia)
 Values
-	()
+	('Airbus A330-900neo',298,23,'TAP'),
+	('Airbus A330-200',263,23,'TAP'),
+	('Airbus A320-200',174,23,'TAP'),
+	('Airbus A330-200',263,23,'TAP');
+
+
+-- cliente
+INSERT INTO cliente(NIF,Nome,Idade,Telemovel,Email,Morada,Nacionalidade,Password)
+Values
+	(111111111,'João',33,'910069859','joao@aquele.com','Naquele Sitio','Portuguesa','12345'),
+	(111111112,'Zé',32,'930005240','joao@aquele.com','No beco','Holandesa','12345'),
+	(111111113,'Quim',63,'960970000','joao@aquele.com','Lyon','Francesa','12345'),
+	(111111114,'Maria',45,'914500000','joao@aquele.com','Lisboa','Portuguesa','12345'),
+	(111111115,'Joana',26,'932340000','joao@aquele.com','Porto','Portuguesa','12345'),
+	(111111116,'Ana',34,'930000968','joao@aquele.com','Lagoa','Portuguesa','12345'),
+	(111111117,'Carl',60,'930888000','joao@aquele.com','Dortmund','Alemã','12345'),
+	(111111118,'Simão',55,'930567000','joao@aquele.com','Londres','Inglesa','12345');
+	
+-- voo
+
+-- bilhete
     
     
