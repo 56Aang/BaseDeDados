abd3d394d6a3a86115fe5cf986a8f5975dee49a7
56Aang <a89588@alunos.uminho.pt>
2020-12-04 21:35:24 +0000

    view

--- view.sql
+++ view.sql
@@ -6,10 +6,11 @@ ORDER BY Nome
 
 CREATE VIEW VistaVoos AS
 SELECT Numero_de_Voo,Hora_de_partida,Hora_de_chegada,Duraçao,Data_de_partida,Origem_id,Destino_id FROM Voo
-ORDER BY Data_de_partida,Hora_de_partida
+ORDER BY Data_de_partida,Hora_de_partida DESC
 
 CREATE VIEW VoosDisponiveis AS
-SELECT Numero_de_Voo,Hora_de_partida,Hora_de_chegada,Duraçao,Data_de_partida,Origem_id,Destino_id
+SELECT Numero_de_Voo,Hora_de_partida,Hora_de_chegada,Duraçao,Data_de_partida,Origem_id,Destino_id,Aeroporto.id,Aeroporto.Nome
 FROM Voo
-WHERE CURRENT_TIMESTAMP() < DATE_ADD(Voo.Data_de_partida, INTERVAL Voo.Hora_de_partida HOUR)
+INNER JOIN Aeroporto ON (Aeroporto.id=Voo.Origem_id ) OR (Aeroporto.id = Voo.Destino_id)
+WHERE (Current_date()=Voo.Data_de_partida AND Current_time()<Voo.Hora_de_Partida) OR (Current_date()<Voo.Data_de_partida)
 
